07706bdb8b2d847c064d254b60f93be0521459c2
aaron <aaronyan2001@gmail.com>
2019-09-08 16:59:53 +0000

    Creating the Category Model, this model hold the categories of the favorite items

--- favorite_things_api/favorite_things_app/models.py
+++ favorite_things_api/favorite_things_app/models.py
@@ -1,6 +1,21 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django_postgres_extensions.models.fields import HStoreField
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+
+class Categories(models.Model):
+    # Ensure the name is unique
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(
+        default="New Category",
+        max_length=100,
+        unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def as_json(self):
+        return dict(name=self.name)
+
+
