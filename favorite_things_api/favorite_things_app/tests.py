81284571a20cdaa6859bf01cab04c0638e0e6e1a
aaron <aaronyan2001@gmail.com>
2019-09-08 18:29:13 +0000

    testing views

--- favorite_things_api/favorite_things_app/tests.py
+++ favorite_things_api/favorite_things_app/tests.py
@@ -1,7 +1,15 @@
 from __future__ import unicode_literals
 from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework.reverse import reverse
 
 from favorite_things_app.models import *
+from favorite_things_app import rest_views
+import json
+
+"""
+Testing Models
+"""
 
 class TestFavoriteModel(TestCase):
     def setUp(self):
@@ -14,3 +22,13 @@ class TestFavoriteModel(TestCase):
         cd = str(self.favorite.created_at).split('.')[0]
         md = str(self.favorite.modified_at).split('.')[0]
         self.assertEquals(cd,md,"Expected modified and created dates to be equal")
+
+"""
+Testing Views
+"""
+class TestFavoriteViewSet(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = rest_views.FavoriteViewSet.as_view({'get': 'list'})
+        self.uri = '/favorites/'
+
